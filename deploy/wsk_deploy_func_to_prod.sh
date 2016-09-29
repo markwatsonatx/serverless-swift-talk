5b1f451e6618be64915e754798ce9e181722f834
Mark Watson <mark.watson.x@gmail.com>
2016-09-29 10:23:51 -0500

    Refactor. Using Jinja2 templating

--- deploy/wsk_deploy_func_to_prod.sh
+++ deploy/wsk_deploy_func_to_prod.sh
@@ -1,9 +1,26 @@
 #!/bin/sh
+absPath() {
+    if [[ -d "$1" ]]; then
+        cd "$1"
+        echo "$(pwd -P)"
+    else 
+        cd "$(dirname "$1")"
+        echo "$(pwd -P)/$(basename "$1")"
+    fi
+}
+
 ./wsk_set_env_prod.sh
 action=$1
 func_name=$2
 func_file_name=$3
 wsk_cmd=$(echo 'wsk action '$action' --kind swift:3')
+mkdir -p ./release/prod
+func_rel_file_name=$(echo $func_file_name | sed 's/.*\///')
+func_rel_file_name=$(echo $func_rel_file_name | sed 's/\.[^.]*$//')
+func_tmp_file_name=$(echo $(dirname $(absPath ${BASH_SOURCE[0]}))'/release/prod/__'$func_rel_file_name'__.swift')
+pushd $(dirname $(absPath $func_file_name))
+j2 $func_file_name > $func_tmp_file_name
+popd
 while read -r line; do
 	param_name=$(echo $line | sed 's/^.*\:[ ]*\(.*\)$/\1/')
 	if [ -n "$param_name" ]
@@ -13,13 +30,6 @@ while read -r line; do
 		wsk_cmd=$(echo $wsk_cmd" --param "$param_name "'"$param_value"'")
 	fi
 done <<< "$(grep -E '\$DefaultParam\:[ ]*.*' $func_file_name)"
-# create temp file for upload
-mkdir -p ./release/prod
-func_rel_file_name=$(echo $func_file_name | sed 's/.*\///')
-func_rel_file_name=$(echo $func_rel_file_name | sed 's/\.[^.]*$//')
-func_tmp_file_name=$(echo './release/prod/__'$func_rel_file_name'__.swift')
-cp $func_file_name $func_tmp_file_name
-./embed_libraries.sh $func_tmp_file_name $func_tmp_file_name
 wsk_cmd=$(echo $wsk_cmd $func_name $func_tmp_file_name)
 echo $wsk_cmd
 eval $wsk_cmd
